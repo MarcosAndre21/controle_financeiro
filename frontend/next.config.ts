361a66ff014cf3bad5039b5370204403a104fec1
MarcosAndre21 <marcosandreramos566@gmail.com>
2026-07-31 14:52:56 -0300

    feat: ajuste sem PWA

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from 'next';
-import withPWAInit from 'next-pwa';
-
-const withPWA = withPWAInit({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
 
 const nextConfig: NextConfig = {
-  // Adiciona a configuração vazia do turbopack para evitar o erro de conflito
-  turbopack: {},
+  /* opções de configuração aqui */
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
